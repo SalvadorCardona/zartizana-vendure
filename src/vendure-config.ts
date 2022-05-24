8d23ca75d03411a4e07870d11c7d1b5aad2f9004
Salva <hebustha@gmail.com>
2022-05-24 17:48:41 +0200

    first commit

---
+++ src/vendure-config.ts
@@ -0,0 +1,92 @@
+import {
+    DefaultJobQueuePlugin,
+    DefaultSearchPlugin,
+    dummyPaymentHandler,
+    VendureConfig,
+} from '@vendure/core';
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import path from 'path';
+
+import fs from 'fs';
+import { ReviewsPlugin } from './plugins/reviews/reviews-plugin';
+import { customAdminUi } from './compile-admin-ui';
+
+const IS_PROD = path.basename(__dirname) === 'dist';
+
+export const config: VendureConfig = {
+
+    apiOptions: {
+      cors: {
+        exposedHeaders: ['vendure-auth-token'],
+      },
+        port: 3000,
+        adminApiPath: 'admin-api',
+        shopApiPath: 'shop-api',
+        adminApiPlayground: {
+            settings: { 'request.credentials': 'include' },
+        },
+        adminApiDebug: true,
+        shopApiPlayground: {
+            settings: { 'request.credentials': 'include' },
+        },
+        shopApiDebug: true,
+    },
+    authOptions: {
+        tokenMethod: 'bearer',
+        cookieOptions: {
+            secret: 'jysakgzhw6',
+        },
+    },
+    dbConnectionOptions: {
+        type: 'postgres',
+        migrations: [getMigrationsPath()],
+        username: 'postgres',
+        password: 'password',
+        database: 'testdb',
+        port: 5432,
+        host: 'database',
+    },
+    paymentOptions: {
+        paymentMethodHandlers: [dummyPaymentHandler],
+    },
+    customFields: {},
+    plugins: [
+        AssetServerPlugin.init({
+            route: 'assets',
+            assetUploadDir: path.join(__dirname, '../static/assets'),
+        }),
+        DefaultSearchPlugin,
+        DefaultJobQueuePlugin,
+        EmailPlugin.init({
+            route: 'mailbox',
+            devMode: true,
+            outputPath: path.join(__dirname, '../static/email/test-emails'),
+            handlers: defaultEmailHandlers,
+            templatePath: path.join(__dirname, '../static/email/templates'),
+            globalTemplateVars: {
+                // The following variables will change depending on your storefront implementation
+                fromAddress: '"example" <noreply@example.com>',
+                verifyEmailAddressUrl: 'http://localhost:8080/verify',
+                passwordResetUrl: 'http://localhost:8080/password-reset',
+                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change',
+            },
+        }),
+        AdminUiPlugin.init({
+            route: 'admin',
+            port: 3002,
+            app: customAdminUi({ recompile: !IS_PROD, devMode: !IS_PROD }),
+        }),
+        ReviewsPlugin,
+    ],
+};
+
+function getMigrationsPath() {
+    const devMigrationsPath = path.join(__dirname, '../migrations');
+    const distMigrationsPath = path.join(__dirname, 'migrations');
+
+    return fs.existsSync(distMigrationsPath)
+        ? path.join(distMigrationsPath, '*.js')
+        : path.join(devMigrationsPath, '*.ts');
+}
